afefa4fa701673ca7d8b29f4f5b47ee1bd8fb5e7
Travis Okonicha <thrinitee@gmail.com>
2022-01-04 16:04:42 +0100

    Initial Commit

---
+++ Tic_Tac_toe/Tic_Tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_toeApp.swift
+//  Tic_Tac_toe
+//
+//  Created by Travis Okonicha on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
